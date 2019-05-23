acc4d4763d4a90aefaed3b48db1078f104ca8e0d
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-05-23 09:34:17 -0300

    Adicionadas as chaves
    Adicionado um Script "KeyManager" que verifica todas as chaves no inicio da simulação, e quando todas são pegas elas desativam a porta

---
+++ Trabalho Bianchini 2019.1/Assets/Scripts/Keys/Key.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+
+    public KeyManager sendInfo;
+
+    private void Start()
+    {
+        sendInfo = GameObject.FindWithTag("KeyManager").GetComponent<KeyManager>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            sendInfo.keysPegas++;
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
---
+++ Trabalho Bianchini 2019.1/Assets/Scripts/Keys/KeyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManager : MonoBehaviour
+{
+    public GameObject[] keys;
+
+    public float keysPegas;
+
+    public GameObject porta;
+
+
+    private void Start()
+    {
+        porta.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if(keysPegas >= keys.Length)
+        {
+            porta.SetActive(false);
+        }
+    }
+
+}
