2c57226746ca075b896982413631ec9be5ad2468
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-05-29 21:39:26 -0300

    Adicionado Informação de "Pressione para interagir"
    Adicionado um canvas que diz "Pressione E para interagir" para interagir com a alavanca

---
+++ Trabalho Bianchini 2019.1/Assets/Scripts/UI/PressE.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressE : MonoBehaviour
+{ 
+    public GameObject texto;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            texto.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            texto.SetActive(false);
+        }
+    }
+}
