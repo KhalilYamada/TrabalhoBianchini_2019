a45635f1710c301a78d03860282253ae46ec9838
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2019-06-13 00:36:47 -0300

    Add Timer
    Adicionei o timer e está completamente funcional, muda de cena para a cena de morte quando termina

---
+++ Trabalho Bianchini 2019.1/Assets/Scripts/Time/TimeManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField]
+    private int tempoFaltandoMin;
+    [SerializeField]
+    private int tempoFaltandoSeg;    
+
+    private int tempoParaPerder;
+
+    public TextMeshProUGUI textoDeTempo;
+
+    private void Start()
+    {
+        StartCoroutine(SubtraiTempo());
+        tempoParaPerder = ((tempoFaltandoMin * 60) + tempoFaltandoSeg) - 1;
+    }
+
+
+    private void Update()
+    {
+        if(tempoFaltandoSeg <= 9)
+        {
+            textoDeTempo.text = "Tempo restante: " + tempoFaltandoMin.ToString() + ":0" + tempoFaltandoSeg.ToString();
+        }
+        else
+        {
+            textoDeTempo.text = "Tempo restante: " + tempoFaltandoMin.ToString() + ":" + tempoFaltandoSeg.ToString();
+        }
+    }
+
+
+
+
+
+    IEnumerator SubtraiTempo()
+    {
+        for (int i = 0; i <= tempoParaPerder; i++)
+        {
+            yield return new WaitForSeconds(1);
+
+            if (tempoFaltandoSeg >= 1)
+            {
+                tempoFaltandoSeg--;
+            }
+            else if (tempoFaltandoMin >= 1)
+            {
+                tempoFaltandoMin--;
+                tempoFaltandoSeg = 59;
+            }
+        }
+        SceneManager.LoadScene("Death");
+    }       
+}
